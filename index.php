9b16fccee098bc000e13f4f106e462dad7cef5d5
Rena3001 <verdiyevarana@gmail.com>
2023-11-04 18:52:00 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,169 @@
+<?php
+
+// define("greeting","Hello");
+// echo greeting;
+
+// const GRETTING ="Hello";
+// echo GRETTING
+
+// echo defined(constant_name:"GRETTING")
+
+// if (true) {
+//     const GRETTING ="Hello";
+
+// }
+
+// echo GRETTING;
+
+
+// if (true) {
+//     define("GRETTING","Hello");
+
+
+// }
+
+// echo GRETTING;
+// gettype();
+// var_dump();
+
+
+// function sum(int $a,string $b){
+//     return $b;
+// }
+
+// echo sum(a:2,b:3);
+// echo sum(a:"5",b:3);
+// echo sum(a:false,b:3);
+//array-->print_r or var_dump() not echo;
+
+// selige  echo "<pre>"
+// echo "</pre>"
+
+
+// count-->uzunluq
+//isset-->include   
+
+//1
+// echo count($arr);
+
+//a
+// echo $arr[count($arr)-1]
+
+// //b
+// echo $arr[0];
+// print_r($arr[0]);
+
+
+//c
+// $countedValues = array_count_values($arr); 
+ 
+// foreach ($countedValues as $key => $value) { 
+//     if ($value > 1) { 
+//         echo "$value"; 
+//     } 
+// }
+
+//d
+// print_r(array_unique($arr)); 
+
+//e
+// $arr[]="salam";
+// print_r ($arr);
+
+//f
+// array_pop($arr);
+// print_r($arr);
+
+//h
+// print_r (array_reverse($arr));
+
+
+// // //2
+// $arr=["php","c++","c#", "python", "java","js","html", "css","react","css"];
+// $arr_2=["html", "css","react","java"];
+// $merge=array_merge($arr,$arr_2);
+// print_r($merge);
+// echo "<pre>";
+// print_r($arr);
+// echo "</pre>";
+
+//a
+// $countedValues = array_count_values($merge); 
+// foreach ($countedValues as $key => $value) { 
+//     if ($value > 1) { 
+//         echo $key; 
+//     } 
+// };
+// print_r($countedValues);
+
+//b
+// array_pop($merge);
+// print_r($merge);
+
+
+//3
+// $arr= [1,2,3,4,5];
+
+//a
+// echo max($arr)
+// echo min($arr)
+
+//b
+// echo array_sum($arr)
+
+//c
+// echo array_product($arr)
+
+//d
+// $arr = array(1,2,3,4,5);  
+// var_dump (implode($arr));
+
+
+//4
+//a
+// $string="Salam, men soz veriremki, php uzre mutexessiz olacam ";
+// $arr =explode(" ", $string);
+// foreach($arr as $v){
+//     echo $v;
+// };
+
+//b
+// echo mb_strtoupper($string);
+
+
+//5
+// $arr=["php","c++","c#", "python", "java","js","html", "css","react","css"];
+// echo "<pre>";
+// var_dump($arr);
+// print_r($arr);
+
+// foreach ($arr as $value) {
+//   echo "$value <br>";
+// }
+// echo "</pre>";
+
+
+// $arr=[];
+// $arr[]="salam";
+// $arr[]="sagol";
+// $arr[]="necesen";
+// print_r($arr);
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////6october//////////////////////
+
+
+
+
+
+
+?>
